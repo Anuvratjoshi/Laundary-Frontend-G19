e3b581c8afc5113e557b72eaf4dd588a5e6bc8f5
Rahul Maurya <rahulmaurya6571@gmail.com>
2023-04-02 15:19:30 +0530

    added order component with styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import './App.css';
-import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom'
-import MainBody from './components/Home/MainBody';
-import { useEffect } from 'react';
+import "./App.css";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import OrderContent from "./components/Order/OrderContent";
+import MainBody from "./components/Home/MainBody";
+import CreateOrder from "./components/Order creation/CreateOrder";
+import Confirmedorder from "./components/past order/Confirmedorder";
+import { useEffect } from "react";
 const Routing = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   useEffect(() => {
-    const user = localStorage.getItem("jwt")
+    const user = localStorage.getItem("jwt");
     if (!user) {
-      navigate("/")
+      navigate("/");
     }
-  },[])
-  return <Routes>
-    <Route path='/' element={<MainBody />} />
-  </Routes>
-}
+  }, []);
+  return (
+    <Routes>
+      <Route path="/" element={<MainBody />} />
+      <Route path="/ordercontent" element={<OrderContent />} />
+      <Route path="/createorder" element={<CreateOrder />} />
+      <Route path="/confirmedorder" element={<Confirmedorder />} />
+    </Routes>
+  );
+};
 
 function App() {
   return (
     <BrowserRouter>
-      <Routing/>
+      <Routing />
     </BrowserRouter>
   );
 }
+
+export default App;
