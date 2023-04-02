cae5abb869ff76c0ae8923857c940dd487361ab8
Anuvrat Joshi <joshianuvrat@gmail.com>
2023-04-02 23:06:14 +0530

    Removed unwanted comments from NavBar.js

--- src/components/Home/NavBar.js
+++ src/components/Home/NavBar.js
@@ -14,32 +14,6 @@ const Navbar = () => {
             </ul>
         </div>
     </nav>
-    {/* <section id="main-body">
-        <section id="laundry-section">
-            <h1>Laundry service</h1>
-            <p>Doorstep Wash & Dryclean Service</p>
-            <h6>Don't Have An Account?</h6>
-            <button style={{float:"left",marginLeft:"10px"}}>Register</button>
-        </section>
-        <section id="form-section">
-            <h3>SIGN IN</h3>
-            
-            <form >
-                <div>
-                    <label htmlFor="mobileNo">Mobile/email</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label>Password</label>
-                    <img src={lock} alt="lock/png" style={{position:"relative",left:"240px",top:"12px"}}/>
-
-                    <input type="password"/>
-                </div>
-                <h6 style={{position:"relative",left:"180px",top:"-15px",color:"#4552C1",fontWeight:"500"}}>forgot password</h6>
-                
-            </form>
-        </section>
-    </section> */}
     </>)
 }
 export default Navbar
